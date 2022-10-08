b481d1d7aeeb8cdc57810fbfd5363be3a20701b7
Jean-Pierre Geslin <jarsop@outlook.com>
2022-10-08 17:15:51 +0200

    (vim.py): use nvim command wrapper to avoid blocking state
    Sometime if your vim instance crash or is in suspended mode
    (e.g: ctrl-Z) the instance can't be reached and the command freeze until
    the instance wake up.
    
    Signed-off-by: Jean-Pierre Geslin <jarsop@outlook.com>

--- alacritty_colorscheme/vim.py
+++ alacritty_colorscheme/vim.py
@@ -1,8 +1,56 @@
+from ctypes import c_long, pythonapi, py_object
 from os import listdir, environ
 from os import stat
 from os.path import isdir, join
 from pynvim import attach
+import queue
 from stat import S_ISSOCK
+import sys
+import threading
+
+
+class NvimCommand(threading.Thread):
+    def __init__(self, instance: str, colorscheme: str, timeout=2):
+        self.timeout = timeout
+        self.exc: queue.Queue = queue.Queue(maxsize=1)
+        super().__init__(
+            target=self._reload,
+            args=(self.exc, instance, colorscheme),
+            daemon=True,
+        )
+        self.start()
+        self.wait_or_kill()
+
+    @staticmethod
+    def _reload(exc: queue.Queue, instance: str, colorscheme_file: str):
+        try:
+            nvim = attach("socket", path=instance)
+            nvim.command(f"source {colorscheme_file}")
+        except:
+            exc.put(sys.exc_info())
+
+    def wait_or_kill(self):
+        self.join(self.timeout)
+        if self.is_alive():
+            self.raise_exception()
+        else:
+            try:
+                exc_type, exc_obj, exc_trace = self.exc.get(False)
+                raise exc_obj.with_traceback(exc_trace)
+            except queue.Empty:
+                pass
+
+    def raise_exception(self):
+        res = pythonapi.PyThreadState_SetAsyncExc(
+            c_long(self.ident), py_object(SystemExit)
+        )
+        if res == 0:
+            raise RuntimeError("Non existent thread id")
+        elif res > 1:
+            pythonapi.PyThreadState_SetAsyncExc(thread_id, 0)
+            raise RuntimeError("Exception raise failure")
+        else:
+            raise RuntimeError("Nvim command timeout")
 
 
 def template_vimrc_background(colorscheme: str) -> str:
@@ -34,15 +82,13 @@ def _get_all_instances():
     return instances
 
 
-def _reload(instance, colorscheme_file):
-    nvim = attach('socket', path=instance)
-    nvim.command(f'source {colorscheme_file}')
-
-
 def reload_neovim_sessions(colorscheme_file):
     instances = _get_all_instances()
-    try:
-        for instance in instances:
-            _reload(instance, colorscheme_file)
-    except Exception:
-        print('Failed loading colorscheme to nvim')
+    for instance in instances:
+        try:
+            NvimCommand(instance, colorscheme_file)
+        except Exception as e:
+            print(
+                f"Failed loading colorscheme `{colorscheme_file}` to nvim session `{instance}`"
+            )
+            print(f"Error: {e}")
